fa19551ac87e2b7072e2dbef13688811ef225a73
Virginia Davenport <virginialdport@gmail.com>
2020-07-10 11:33:52 -0600

    created react app

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express")
+const cors = require("cors")
+
 const server = express();
 
 const projectRouter = require("./projects/projectRouter");
 const actionRouter = require("./actions/actionRouter")
 
 server.use(express.json())
+server.use(cors())
+
 server.use("/api/projects", projectRouter)
 server.use("/api/actions", actionRouter)
 
